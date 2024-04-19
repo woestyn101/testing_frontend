0a64f072ffffad6fb0b647e3b40911556be722e9
Marius Du Plooy <woestyn101@gmail.com>
2024-04-18 20:00:35 -0400

    second

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <div className='pages'>
+        <Routes>
+          <Route path="/"
+          element={<Home></Home>}>
+
+          </Route>
+
+        </Routes>
+
+      </div>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
